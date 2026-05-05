b3fa5ce9524e1fb131a6622ce9fab878f76b66df
ckinan <cesar.kina@gmail.com>
2026-05-04 22:18:59 -0500

    use custom theme grayscale ckgrayscale.nvim

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,10 +1,10 @@
 return {
-  "zenbones-theme/zenbones.nvim",
+  "ckinan/ckgrayscale.nvim",
   dependencies = { "rktjmp/lush.nvim" },
   lazy = false,
   priority = 1000,
   config = function()
-    vim.opt.background = "light"
-    vim.cmd.colorscheme("zenbones")
+    vim.opt.background = "dark"
+    vim.cmd.colorscheme("ckgrayscale")
   end,
 }
